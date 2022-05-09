ff9629d136eb7b6de99984059ec827813589f8d1
NAMORALES <>
2022-05-09 16:10:51 +0000

    ejemplo en clase

---
+++ Trans-Referencial/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int a=1;
+
+int funcion(int Numini){
+  a = a + 1;
+  int finNum = Numini * Numini;
+return finNum;
+}
+
+int main() {
+  int b=2;
+  int c=3;
+  cout<<((a+2*funcion(b))*(c+2*funcion(b)))<<endl;
+  cout<<a<<endl;
+  cout<<((a+2*funcion(b))*(c+2*funcion(b)))<<endl;
+  cout<<a<<endl;
+}
